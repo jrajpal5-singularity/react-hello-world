e75c9a60f4cc7d4ba413fc688e579fe43b486696
jimmy.rajpal <jrajpal5@gmail.com>
2023-04-06 13:21:48 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
-
+// import {Greet} from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet></Greet>
+      <Greet />
+      <Welcome/>
 
     </div>
   );
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -6,6 +6,6 @@ import React from "react";
 // writing with es6
 const Greet = () => <h1>hello jimmy!</h1>
 
-
+// export const Greet = () => <h1>hello jimmy!</h1>
 
 export default Greet
