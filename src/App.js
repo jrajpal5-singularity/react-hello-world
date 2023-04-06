4d99b8ca3938a310c19c3af5088542eb0b7c7ebb
jimmy.rajpal <jrajpal5@gmail.com>
2023-04-06 14:08:07 +0530

    jsx

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 // import {Greet} from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome/>
+      {/* <Greet />
+      <Welcome/> */}
 
+      <Hello/>
     </div>
   );
 }
