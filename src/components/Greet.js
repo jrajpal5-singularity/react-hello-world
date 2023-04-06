c0c5fb3d392cfb422f283dfe1e7d66b8629fd0e1
jimmy.rajpal <jrajpal5@gmail.com>
2023-04-06 13:13:59 +0530

    added components

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1> Hello jimmy</h1>
+// }
+// writing with es6
+const Greet = () => <h1>hello jimmy!</h1>
+
+
+
+export default Greet
