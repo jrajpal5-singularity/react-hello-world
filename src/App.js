33d74ea0f57749691911c4bf9f0424523c057a7b
jimmy.rajpal <jrajpal5@gmail.com>
2023-04-06 16:20:10 +0530

    state

--- src/App.js
+++ src/App.js
@@ -3,30 +3,43 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import { Component } from 'react';
+import Message from './components/Message';
 
 // import {Greet} from './components/Greet';
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <Greet name="bruce" heroName="Batman">
+//        <p> This is children props</p>
+//       </Greet>
+//       <Greet name="clark" heroName="superman">
+//         <button>
+//           Action
+//         </button>
+//         </Greet>
+//       <Greet name="berry" heroName="Flash"/>
+//       <Welcome name="bruce" heroName="Batman"/>
+
+
+
+//       {/* <Greet />
+//       <Welcome/> */}
+
+//       {/* <Hello/> */}
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <Greet name="bruce" heroName="Batman">
-       <p> This is children props</p>
-      </Greet>
-      <Greet name="clark" heroName="superman">
-        <button>
-          Action
-        </button>
-        </Greet>
-      <Greet name="berry" heroName="Flash"/>
-      <Welcome name="bruce" heroName="Batman"/>
+      <Message> </Message>
 
-
-
-      {/* <Greet />
-      <Welcome/> */}
-
-      {/* <Hello/> */}
     </div>
   );
+}
 }
 
 export default App;
