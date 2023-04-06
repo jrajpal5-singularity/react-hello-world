b77ca0eee2b3323b72d48f53bfd44503af0a4cac
jimmy.rajpal <jrajpal5@gmail.com>
2023-04-06 14:43:05 +0530

    jsx

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,20 @@
 import React from "react";
 
+// with jsx coding becomes easy like html
+// const Hello = () => {
+//     return (
+//         <div>
+//             <h1>hello hello jimmy</h1>
+//         </div>
+//     )
+// }
+
+// without jsx
 const Hello = () => {
-    return (
-        <div>
-            <h1>hello hello jimmy</h1>
-        </div>
+    return React.createElement('div',{id:'hello',className:'dummyClass'},React.createElement('h1',null,'hello hello jimmy')
     )
 }
 
-export default Hello;
+
+
+export default Hello
