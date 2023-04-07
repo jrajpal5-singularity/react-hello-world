a394486bc0c8218467f124ce0b06de40a0cfc6e9
jimmy.rajpal <jrajpal5@gmail.com>
2023-04-07 10:28:05 +0530

    event handling2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Hello from './components/Hello';
 import { Component } from 'react';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 // import {Greet} from './components/Greet';
 // function App() {
@@ -36,8 +38,8 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-      
-      <Counter/>
+      <FunctionClick/>
+      <ClassClick/>
 
     </div>
   );
